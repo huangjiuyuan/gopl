ca3d8026687609eee98e4458ea37630ccc5b8f49
huangjiuyuan <jiuyuan.huang@daocloud.io>
2017-10-20 20:56:09 +0800

    multiplexing with select

---
+++ 08/multiplexingwithselect/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x) // "0" "2" "4" "6" "8"
+		case ch <- i:
+		}
+	}
+
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown. Press return to abort.")
+	select {
+	case <-time.After(5 * time.Second):
+		// Do nothing.
+	case <-abort:
+		fmt.Println("Launch aborted!")
+		return
+	}
+	launch()
+}
+
+func launch() {
+	fmt.Println("Rocket launched!")
+}
