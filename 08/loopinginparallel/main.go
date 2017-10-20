7868b642280abee55ef288df54fce4cc436f97d9
huangjiuyuan <jiuyuan.huang@daocloud.io>
2017-10-20 15:41:54 +0800

    looping in parallel

---
+++ 08/loopinginparallel/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/huangjiuyuan/gopl/08/loopinginparallel/thumbnail"
+)
+
+func main() {
+	filenames := []string{"file_0", "file_1", "file_2", "file_3", "file_4"}
+	thumbfiles, _ := makeThumbnails(filenames)
+	fmt.Println(thumbfiles)
+}
+
+func makeThumbnails(filenames []string) (thumbfiles []string, err error) {
+	type item struct {
+		thumbfile string
+		err       error
+	}
+
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thumbfile, it.err = thumbnail.ImageFile(f)
+			ch <- it
+		}(f)
+	}
+
+	for range filenames {
+		it := <-ch
+		if it.err != nil {
+			return nil, it.err
+		}
+		thumbfiles = append(thumbfiles, it.thumbfile)
+	}
+
+	return thumbfiles, nil
+}
+
+func makeThumbnails2(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup // number of working goroutines
+	for f := range filenames {
+		wg.Add(1)
+		// worker
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb) // OK to ignore error
+			sizes <- info.Size()
+		}(f)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+	return total
+}
